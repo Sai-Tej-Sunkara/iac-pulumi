d44dda9bd5d545f4b93ee6b561f58315f944fd8e
sstej88 <sunkara.sai@northeastern.edu>
2023-10-12 12:42:05 -0400

    Updated 10.0.0.0/16 Code

--- index.js
+++ index.js
@@ -1,64 +1,47 @@
 "use strict";
 const pulumi = require("@pulumi/pulumi");
 const aws = require("@pulumi/aws");
-const awsx = require("@pulumi/awsx");
 
 const vpc = new aws.ec2.Vpc("webapp-vpc", {
     cidrBlock: "10.0.0.0/16",
 });
 
-// console.log("VPC")
-// console.log(vpc)
-
 const internetGateway = new aws.ec2.InternetGateway("vpc-internet-gateway", {
     vpcId: vpc.id,
 });
-
-// console.log("Internet Gateway")
-// console.log(internetGateway)
 
 const availabilityZones = pulumi.output(aws.getAvailabilityZones({})).apply(azs => azs.names);
 
 availabilityZones.apply(availabilityZone => {
     const totalZones = availabilityZone.length;
-    // console.log("Avaialbility Zones")
-    // console.log(totalZones)
 
     var createSubnets = (type, offsetStart) => {
-        const subnets = []
-        let cidrOffset = offsetStart
+        const subnets = [];
+        let cidrOffset = offsetStart;
 
-        for(let i=0; i<3; i++) {
+        for (let i = 0; i < 3; i++) {
             const availabilityZoneIndex = i % totalZones;
-            subnets.push(new aws.ec2.Subnet(type+"-subnet-"+i.toString(), {
+            subnets.push(new aws.ec2.Subnet(type + "-subnet-" + i.toString(), {
                 vpcId: vpc.id,
-                cidrBlock: "10.0."+cidrOffset.toString()+".0/24",
+                cidrBlock: `10.0.${i * 10 + cidrOffset}.0/24`,
                 availabilityZone: availabilityZone[availabilityZoneIndex],
                 mapPublicIpOnLaunch: type === "public",
             }));
-            cidrOffset += totalZones * 2;
+            cidrOffset += 1;
         }
 
         return subnets;
     }
 
     const publicSubnets = createSubnets("public", 0);
-    const privateSubnets = createSubnets("private", 1);
-
-    // console.log("Public Subnets")
-    // console.log(publicSubnets)
-    // console.log("Private Subnets")
-    // console.log(privateSubnets)
+    const privateSubnets = createSubnets("private", 3);
 
     const publicRouteTable = new aws.ec2.RouteTable("webapp-public-route-table", {
         vpcId: vpc.id,
     });
 
-    // console.log("Public Route Table")
-    // console.log(publicRouteTable)
-
     publicSubnets.forEach((subnet, i) => {
-        new aws.ec2.RouteTableAssociation("public-route-table-association-"+i.toString(), {
+        new aws.ec2.RouteTableAssociation("public-route-table-association-" + i.toString(), {
             routeTableId: publicRouteTable.id,
             subnetId: subnet.id,
         });
@@ -74,11 +57,8 @@ availabilityZones.apply(availabilityZone => {
         vpcId: vpc.id,
     });
 
-    // console.log("Private Route Table")
-    // console.log(privateRouteTable)
-
     privateSubnets.forEach((subnet, i) => {
-        new aws.ec2.RouteTableAssociation("private-route-table-association-"+i.toString(), {
+        new aws.ec2.RouteTableAssociation("private-route-table-association-" + i.toString(), {
             routeTableId: privateRouteTable.id,
             subnetId: subnet.id,
         });
