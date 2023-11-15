67e2e1f52d80f14eaed159d3ad3f3414f6616cf9
sstej88 <sunkara.sai@northeastern.edu>
2023-11-15 03:24:56 -0500

    Updated comments

--- index.js
+++ index.js
@@ -145,20 +145,20 @@ availabilityZones.apply(async (availabilityZone) => {
     ingress: [
       {
         protocol: "tcp",
-        fromPort: 80, // HTTP
+        fromPort: 80,
         toPort: 80,
         cidrBlocks: ["0.0.0.0/0"],
       },
       {
         protocol: "tcp",
-        fromPort: 443, // HTTPS
+        fromPort: 443,
         toPort: 443,
         cidrBlocks: ["0.0.0.0/0"],
       },
     ],
     egress: [
       {
-        protocol: "-1", // Allow all outbound traffic
+        protocol: "-1",
         fromPort: 0,
         toPort: 0,
         cidrBlocks: ["0.0.0.0/0"],
